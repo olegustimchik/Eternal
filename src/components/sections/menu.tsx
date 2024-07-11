078a748b5fece3a29b5b74aaf3e1b56f0ccc72c8
oleh <olegustimcik84@gmail.com>
2024-07-11 19:44:35 +0300

    create separeted component Menu

--- src/components/sections/menu.tsx
+++ src/components/sections/menu.tsx
@@ -3,9 +3,10 @@ import { FC, Dispatch, SetStateAction } from "react";
 
 type MenuProps =  {
   setOpen: Dispatch<SetStateAction<boolean>>;
+  isOpen : boolean;
 };
 
-export const Menu: FC<MenuProps> = ({ setOpen }) => {
+export const Menu: FC<MenuProps> = ({ isOpen, setOpen }) => {
   return (
     <div className="menu">
       <div className="menu-button" onClick={() => setOpen(true)}>
