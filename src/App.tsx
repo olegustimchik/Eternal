2836b111af082c2f3a442f5e0391b46ed7c74f96
oleh <olegustimcik84@gmail.com>
2024-07-16 18:58:38 +0300

    add main portrets on home-page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import { Header }      from "./components/layers/header";
-import { EternalLogo } from "./components/logo/eternal-logo";
+import { HomePage } from "./components/pages/home";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Header></Header>
-      <div style={ {
-        height: "400px", width: "400px", position: "absolute", top: "200px", left: "200px",
-      }}>
-        <EternalLogo></EternalLogo>
-      </div>
+      <HomePage></HomePage>
     </>
   );
 }
---
+++ src/components/pages/home.tsx
@@ -0,0 +1,46 @@
+import "@/assets/styles/pages/home.css";
+import HomePageBackground from "@public/HomePageBackground-crop-ddd05278.png";
+import Albert             from "@public/albert-f3553b03.png";
+import conversation       from "@public/conversation-crop-c49be2c0.png";
+import Elon               from "@public/elon-de8cf9c9.png";
+import King               from "@public/king-d8f4da2d.png";
+import Steven             from "@public/steve-bcb8516d.png";
+import Tereza             from "@public/tereza-6dde73d3.png";
+
+import { Header }         from "../layers/header";
+
+export function HomePage() {
+  return (
+    <>
+      <Header></Header>
+      {/* <div style={ {
+        height: "100px", width: "100px", position: "absolute", top: "200px", left: "200px",
+      }}>
+        <EternalLogo></EternalLogo> */}
+      <div className="home-container">
+        <div className="text-container">
+          <h1 className="ask-text">ask important people important questions</h1>
+          <p className="text-after-ask">Choose a question to quickly get a realistic response</p>
+        </div>
+        <div className="home-message-container">
+          <button className="home-message-button"><span className="home-message-text-container"><p className="home-message-text">What did you want to be when you grew up?</p></span></button>
+          <button className="home-message-button"><span className="home-message-text-container"><p className="home-message-text">What is the meaning of life?</p></span></button>
+          <button className="home-message-button"><span className="home-message-text-container"><p className="home-message-text">What is your greatest accomplishment?</p></span></button>
+        </div>
+        <div className="background-container">
+          <div className="conversation">
+            <img src={conversation} alt="conversation"></img>
+          </div>
+          <img className="home-page-back-image" src={HomePageBackground} alt="backGround"></img>
+          <img className="king-img" src={King} alt="Luter King"></img>
+          <img className="albert-img" src={Albert} alt="Albert Enshtein"></img>
+          <img className="steven-img" src={Steven} alt="Steven"></img>
+          <img className="tereza-img" src={Tereza} alt="Tereza"></img>
+          <img className="elon-img" src={Elon} alt="Elon Mask"></img>
+          <div className="under-images"> </div>
+        </div>
+      </div>
+
+    </>
+  );
+}
